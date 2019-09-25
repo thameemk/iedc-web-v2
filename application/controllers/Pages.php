9491d7ecef412c902ec5c6a106ead183989c7a29
thameemk612 <thameemk612@gmail.com>
2019-09-25 13:59:20 +0530

    theme & environment

---
+++ application/controllers/Pages.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+   function __construct() {
+      parent::__construct();
+      $this->load->library('form_validation');
+      $this->load->model('report_model');
+      $this->load->helper(array('form', 'url'));
+    }
+
+	public function index()
+	{
+        $data['page_title'] = 'Home';    
+		$this->load->view('templates/header',$data);
+		$this->load->view('static/home',$data);
+		$this->load->view('templates/footer');
+	}
+ 
+    function view($page){
+      if ( ! file_exists(APPPATH.'views/static/'.$page.'.php')){
+          show_404();
+      }
+      $temp = ucfirst($page);
+      $data['page_title'] = $temp;     
+      $this->load->view('templates/header',$data);
+      $this->load->view('static/'.$page,$data);
+      $this->load->view('templates/footer');
+    }
+
+}
---
+++ application/models/Report_model.php
@@ -0,0 +1,8 @@
+<?php
+class Report_model extends CI_Model {
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+}
