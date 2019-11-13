54739603530d5b51bf2c253335c7e94be351a318
amjadamju <amjadpanjeeri@gmail.com>
2019-11-13 21:58:34 +0530

    multi-stories.php

---
+++ application/views/static/multi-stories.php
@@ -0,0 +1,92 @@
+
+        <section id="page-title" data-parallax-image="polo/images/parallax/5.jpg">
+            <div class="container">
+                <div class="page-title">
+                    <h1>Stories</h1>
+                </div>
+                <div class="breadcrumb">
+                    <ul>
+                        <li><a href="<?=base_url()?>">Home</a>
+                        </li>
+                        </li>
+                        <li class="active"><a href="<?=base_url()?>">IEDC</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        
+
+        <section id="page-content">
+            <div class="container">
+                <div id="blog" class="grid-layout post-3-columns m-b-30" data-item="post-item">
+
+                    <div class="post-item border">
+                        <div class="post-item-wrap">
+                            <div class="post-image">
+                                <a href="#">
+                                    <img alt="" src="<?=base_url()?>../theme/polo/images/blog/12.jpg">
+                                </a>
+                                <span class="post-meta-category"><a href="#">Lifestyle</a></span>
+                            </div>
+                            <div class="post-item-description">
+                                <span class="post-meta-date"><i class="fa fa-calendar-o"></i>Jan 21, 2017</span>
+                                <span class="post-meta-comments"><a href="#"><i class="fa fa-comments-o"></i>33
+                                        Comments</a></span>
+                                <h2><a href="#">Standard post with a single image
+                                    </a></h2>
+                                <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
+                                    dolor porta feugiat. Fusce at velit id ligula pharetra laoreet.</p>
+                                <a href="#" class="item-link">Read More <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="post-item border">
+                        <div class="post-item-wrap">
+                            <div class="post-image">
+                                <a href="#">
+                                    <img alt="" src="<?=base_url()?>../theme/polo/images/blog/12.jpg">
+                                </a>
+                                <span class="post-meta-category"><a href="#">Lifestyle</a></span>
+                            </div>
+                            <div class="post-item-description">
+                                <span class="post-meta-date"><i class="fa fa-calendar-o"></i>Jan 21, 2017</span>
+                                <span class="post-meta-comments"><a href="#"><i class="fa fa-comments-o"></i>33
+                                        Comments</a></span>
+                                <h2><a href="#">Standard post with a single image
+                                    </a></h2>
+                                <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
+                                    dolor porta feugiat. Fusce at velit id ligula pharetra laoreet.</p>
+                                <a href="#" class="item-link">Read More <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="post-item border">
+                        <div class="post-item-wrap">
+                            <div class="post-image">
+                                <a href="#">
+                                    <img alt="" src="<?=base_url()?>../theme/polo/images/blog/12.jpg">
+                                </a>
+                                <span class="post-meta-category"><a href="#">Lifestyle</a></span>
+                            </div>
+                            <div class="post-item-description">
+                                <span class="post-meta-date"><i class="fa fa-calendar-o"></i>Jan 21, 2017</span>
+                                <span class="post-meta-comments"><a href="#"><i class="fa fa-comments-o"></i>33
+                                        Comments</a></span>
+                                <h2><a href="#">Standard post with a single image
+                                    </a></h2>
+                                <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
+                                    dolor porta feugiat. Fusce at velit id ligula pharetra laoreet.</p>
+                                <a href="#" class="item-link">Read More <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
