2d8a0d68ee0c75a1db97d47581ebe1ef7613b26b
amjadamju <amjadpanjeeri@gmail.com>
2019-11-13 22:57:06 +0530

    modified login.php

--- application/views/static/login.php
+++ application/views/static/login.php
@@ -1,27 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="author" content="INSPIRO" />
-    <meta name="description" content="Themeforest Template Polo">
-
-    <title>IEDC | Login</title>
-    <link href="polo/css/plugins.css" rel="stylesheet">
-    <link href="polo/css/style.css" rel="stylesheet">
-    <link href="polo/css/responsive.css" rel="stylesheet">
-</head>
-
-<body>
-    <div class="body-inner">
-
         <section class="fullscreen">
             <div class="container container-fullscreen">
                 <div class="text-middle">
                     <div class="text-center m-b-30">
                         <a href="<?=base_url()?>" class="logo">
-                            <img src="<?=base_url()?>../theme/images/logo.png" alt="IEDC Logo">
+                            <img src="<?=base_url()?>/theme/images/logo.png" alt="IEDC Logo">
                         </a>
                     </div>
                     <div class="row">
@@ -54,18 +36,3 @@
                 </div>
             </div>
         </section>
-
-    </div>
-
-
-    <a id="scrollTop"><i class="icon-chevron-up1"></i><i class="icon-chevron-up1"></i></a>
-
-    <script src="js/jquery.js"></script>
-    <script src="js/plugins.js"></script>
-
-    <script src="js/functions.js"></script>
-</body>
-
-<!-- Mirrored from www.inspirothemes.com/polo/page-user-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 21 Sep 2019 08:06:02 GMT -->
-
-</html>
