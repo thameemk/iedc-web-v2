bf4eb48ac0ad33cec4b85a42195e4f77af43ee24
amjadamju <amjadpanjeeri@gmail.com>
2019-11-13 22:33:31 +0530

    Added login.php

---
+++ theme/login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="INSPIRO" />
+    <meta name="description" content="Themeforest Template Polo">
+
+    <title>IEDC | Login</title>
+    <link href="polo/css/plugins.css" rel="stylesheet">
+    <link href="polo/css/style.css" rel="stylesheet">
+    <link href="polo/css/responsive.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="body-inner">
+
+        <section class="fullscreen">
+            <div class="container container-fullscreen">
+                <div class="text-middle">
+                    <div class="text-center m-b-30">
+                        <a href="<?=base_url()?>" class="logo">
+                            <img src="<?=base_url()?>../theme/images/logo.png" alt="IEDC Logo">
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-5 center p-50 background-white b-r-6">
+                            <h3>Login to your Account</h3>
+                            <form>
+                                <div class="form-group">
+                                    <label class="sr-only">Username or Email</label>
+                                    <input type="text" class="form-control" placeholder="Username or Email">
+                                </div>
+                                <div class="form-group m-b-5">
+                                    <label class="sr-only">Password</label>
+                                    <input type="password" class="form-control" placeholder="Password">
+                                </div>
+                                <div class="form-group form-inline text-left">
+                                    <div class="form-check">
+                                        <label>
+                                            <input type="checkbox"><small class="m-l-10"> Remember me</small>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="text-left form-group">
+                                    <button type="button" class="btn">Login</button>
+                                </div>
+                            </form>
+                            <p class="small">Don't have an account yet? <a href="#">Register New Account</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+
+    <a id="scrollTop"><i class="icon-chevron-up1"></i><i class="icon-chevron-up1"></i></a>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.js"></script>
+
+    <script src="js/functions.js"></script>
+</body>
+
+<!-- Mirrored from www.inspirothemes.com/polo/page-user-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 21 Sep 2019 08:06:02 GMT -->
+
+</html>
