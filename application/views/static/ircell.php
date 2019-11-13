b44006651d9f03b5cb96ee681a5c16c94312a561
amjadamju <amjadpanjeeri@gmail.com>
2019-11-13 18:04:33 +0530

    ircell.php

---
+++ application/views/static/ircell.php
@@ -0,0 +1,42 @@
+        <section id="page-title" data-parallax-image="<?=base_url()?>">
+            <div class="container">
+                <div class="page-title">
+                    <h1>IR-Cell</h1>
+                </div>
+                <div class="breadcrumb">
+                    <ul>
+                        <li><a href="<?=base_url()?>">Home</a> </li>
+
+                        <li class="active"><a href="<?=base_url()?>ircell">IR-Cell</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+
+        <section id="page-content">
+            <div class="container">
+
+                <div class="page-title">
+
+                    <div class="breadcrumb float-left">
+
+                    </div>
+                </div>
+
+
+                <div id="blog" class="post-thumbnails">
+
+                    <div class="post-item">
+                        <div class="post-item-wrap">
+                            <h2>Innovation & Research Cell</h2>
+                            <p>Every fabulous invention that we have come across results form a thought that wandered
+                                out of the box. Innovation & research cell is a community which aims to foster such
+                                research culture in the young budding minds of the student community, through
+                                mentorships and support in all respects. The community strives to build a generation
+                                brimming with innovation ideas for a better tomorrow.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
