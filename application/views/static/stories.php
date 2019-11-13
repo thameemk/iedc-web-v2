727680b9524964b6a25e5e4211806d151815c02e
MuhammedRizankp <muhammedrizan10@gmail.com>
2019-11-13 18:00:57 +0530

    Added stories.php

---
+++ application/views/static/stories.php
@@ -0,0 +1,99 @@
+<section id="page-content" class="sidebar-right">
+    <div class="container">
+        <div class="row">
+
+            <div class="content col-lg-12">
+
+                <div id="blog" class="single-post">
+
+                    <div class="post-item">
+                        <div class="post-item-wrap">
+                            <div class="post-image">
+                                <a href="#">
+                                    <img alt="" src="<?=base_url()?>assets/uploads/stories/images/1.jpg">
+                                </a>
+                            </div>
+                            <div class="post-item-description">
+                                <h2>Standard post with a single image</h2>
+                                <div class="post-meta">
+                                    <span class="post-meta-date"><i class="fa fa-calendar-o"></i>Jan 21,
+                                        2017</span>
+                                    <div class="post-meta-share">
+                                        <a class="btn btn-xs btn-slide btn-facebook" href="#">
+                                            <i class="fab fa-facebook-f"></i>
+                                            <span>Facebook</span>
+                                        </a>
+                                        <a class="btn btn-xs btn-slide btn-twitter" href="#" data-width="100">
+                                            <i class="fab fa-twitter"></i>
+                                            <span>Twitter</span>
+                                        </a>
+                                        <a class="btn btn-xs btn-slide btn-instagram" href="#" data-width="118">
+                                            <i class="fab fa-instagram"></i>
+                                            <span>Instagram</span>
+                                        </a>
+                                        <a class="btn btn-xs btn-slide btn-googleplus" href="mailto:#" data-width="80">
+                                            <i class="far fa-envelope"></i>
+                                            <span>Mail</span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus
+                                    commodo dolor porta feugiat. Fusce at velit id ligula pharetra laoreet. Ut
+                                    nec metus a mi ullamcorper hendrerit. Nulla facilisi. Pellentesque sed nibh
+                                    a quam accumsan dignissim quis quis urna. The most happiest time of the
+                                    day!. Praesent id dolor dui, dapibus gravida elit. Donec consequat laoreet
+                                    sagittis. Suspendisse ultricies ultrices viverra. Morbi rhoncus laoreet
+                                    tincidunt. Mauris interdum convallis metus.M</p>
+                                <div class="blockquote">
+                                    <p>The world is a dangerous place to live; not because of the people who are
+                                        evil, but because of the people who don't do anything about it.</p>
+                                    <small>by <cite>Albert Einstein</cite></small>
+                                </div>
+                                <p> The most happiest time of the day!. Praesent id dolor dui, dapibus gravida
+                                    elit. Donec consequat laoreet sagittis. Suspendisse ultricies ultrices
+                                    viverra. Morbi rhoncus laoreet tincidunt. Mauris interdum convallis metus.
+                                    Suspendisse vel lacus est, sit amet tincidunt erat. Etiam purus sem, euismod
+                                    eu vulputate eget, porta quis sapien. Donec tellus est, rhoncus vel
+                                    scelerisque id, iaculis eu nibh.</p>
+                                <p>Donec posuere bibendum metus. Quisque gravida luctus volutpat. Mauris
+                                    interdum, lectus in dapibus molestie, quam felis sollicitudin mauris, sit
+                                    amet tempus velit lectus nec lorem. Nullam vel mollis neque. The most
+                                    happiest time of the day!. Nullam vel enim dui. Cum sociis natoque penatibus
+                                    et magnis dis parturient montes, nascetur ridiculus mus. Sed tincidunt
+                                    accumsan massa id viverra. Sed sagittis, nisl sit amet imperdiet convallis,
+                                    nunc tortor consequat tellus, vel molestie neque nulla non ligula. Proin
+                                    tincidunt tellus ac porta volutpat. Cras mattis congue lacus id bibendum.
+                                    Mauris ut sodales libero. Maecenas feugiat sit amet enim in accumsan.</p>
+                                <p>Duis vestibulum quis quam vel accumsan. Nunc a vulputate lectus. Vestibulum
+                                    eleifend nisl sed massa sagittis vestibulum. Vestibulum pretium blandit
+                                    tellus, sodales volutpat sapien varius vel. Phasellus tristique cursus erat,
+                                    a placerat tellus laoreet eget. Fusce vitae dui sit amet lacus rutrum
+                                    convallis. Vivamus sit amet lectus venenatis est rhoncus interdum a vitae
+                                    velit.</p>
+                            </div>
+                            <div class="post-tags">
+                                <a href="#">Life</a>
+                                <a href="#">Sport</a>
+                                <a href="#">Tech</a>
+                                <a href="#">Travel</a>
+                            </div>
+                            <div class="post-navigation">
+                                <a href="#" class="post-prev">
+                                    <div class="post-prev-title"><span>Previous Post</span>Post with a slider
+                                        and lightbox</div>
+                                </a>
+                                <a href="#" class="post-all">
+                                    <i class="icon-grid"> </i>
+                                </a>
+                                <a href="#" class="post-next">
+                                    <div class="post-next-title"><span>Next Post</span>Post with YouTube Video
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
