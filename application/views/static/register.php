3d99821dbb501c0d4d9a2ee67230da2452a8296f
amjadamju <amjadpanjeeri@gmail.com>
2019-11-13 17:54:33 +0530

    register.php

---
+++ application/views/static/register.php
@@ -0,0 +1,125 @@
+<section id="page-title" data-parallax-image="<?= base_url() ?>images/parallax/5.jpg">
+    <div class="container">
+        <div class="page-title">
+            <h1>Register</h1>
+
+        </div>
+        <div class="breadcrumb">
+            <ul>
+                <li><a href="<?= base_url() ?>">Home</a>
+                </li>
+
+                </li>
+                <li class="active"><a href="<?= base_url() ?>register">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 center no-padding">
+                <form class="form-transparent-grey">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h3>Register Here</h3>
+                            <br>
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            <label class="sr-only">First Name</label>
+                            <input type="text" value="" placeholder="First Name" class="form-control">
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            <label class="sr-only">Last Name</label>
+                            <input type="text" value="" placeholder="Last Name" class="form-control">
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            <label class="sr-only">Email</label>
+                            <input type="text" value="" placeholder="Email" class="form-control">
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            <label class="sr-only">Phone</label>
+                            <input type="" value="" placeholder="Phone" class="form-control">
+                        </div>
+                        <div class="col-lg-12 form-group">
+                            <div class="form-group">
+
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option value="">Branch</option>
+                                    <option>M-Tech</option>
+                                    <option>Architecture</option>
+                                    <option>Chemical Engineering</option>
+                                    <option>Civil Engineering</option>
+                                    <option>Computer Science & Engineering</option>
+                                    <option>Electronics & Communication Engineering</option>
+                                    <option>Electrical & Electronics Engineering</option>
+                                    <option>Master of Computer Application</option>
+                                    <option>Mechanical Engineering</option>
+                                    <option>Mechanical Production</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            <label class="sr-only">Password</label>
+                            <input type="password" value="" placeholder="Password" class="form-control">
+                        </div>
+
+                        <div class="col-lg-6 form-group">
+                            <label class="sr-only">Password</label>
+                            <input type="password" value="" placeholder=" Confirm Password" class="form-control">
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            </b><label><b>Course Duration</b></label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option value="">From</option>
+                                <option>2015</option>
+                                <option>2016</option>
+                                <option>2017</option>
+                                <option>2018</option>
+                                <option>2019</option>
+
+                            </select>
+                        </div>
+
+                        <div class="col-lg-6 form-group" style="margin-top: 27px;">
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option value="">To</option>
+                                <option>2019</option>
+                                <option>2020</option>
+                                <option>2021</option>
+                                <option>2022</option>
+                                <option>2023</option>
+
+                            </select>
+                        </div>
+
+                        <div class="col-lg-6 form-group">
+                            <label class="sr-only">Admission Number</label>
+                            <input type="text" value="" placeholder="Admission Number" class="form-control">
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+
+                                <div class="input-group date" id="datetimepicker2" data-target-input="nearest">
+                                    <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker2" placeholder="Date of Birth" />
+                                    <div class="input-group-append" data-target="#datetimepicker2" data-toggle="datetimepicker">
+                                        <div class="input-group-text"><i class="icon-calendar21"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-12">
+                            <div class="form-group">
+
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Why IEDC ?"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 form-group">
+                            <button class="btn" type="button">SUBMIT</button>
+                        </div>
+                    
+</section>
