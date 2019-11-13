b24fa4ad652886f40b751f39780c59df2d965136
thameemk612 <thameemk612@gmail.com>
2019-11-13 19:21:04 +0530

    community & ecell

---
+++ application/views/static/community.php
@@ -0,0 +1,62 @@
+<section id="page-title" data-parallax-image="polo/images/parallax/5.jpg">
+	<div class="container">
+		<div class="page-title">
+			<h1>Community</h1>
+		</div>
+		<div class="breadcrumb">
+			<ul>
+				<li><a href="<?=base_url()?>">Home</a> </li>
+
+				<li class="active"><a href="<?=base_url()?>community">Community</a> </li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+
+
+<section id="page-content">
+	<div class="container">
+
+		<div class="page-title">
+
+			<div class="breadcrumb float-left">
+
+			</div>
+		</div>
+
+
+		<div id="blog" class="post-thumbnails">
+
+			<div class="post-item mb-5">
+				<div class="post-item-wrap">
+					<div class="post-image">
+						<a href="#">
+							<img src="<?=base_url()?>assets/front/images/community/open-source.png" alt="" class="img-fluid" alt="open-source" class="img-fluid" style="height: 150px ; width: 200px;">
+						</a>
+					</div>
+					<div class="post-item-description">
+						<h2><a href="#">Open Source Community
+							</a></h2>
+						<p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo dolor porta feugiat. Fusce at velit id ligula pharetra laoreet.</p>
+					</div>
+				</div>
+			</div>
+
+			<hr>
+			<div class="post-item mt-5">
+				<div class="post-item-wrap">
+					<div class="post-image">
+						<a href="#">
+							<img src="<?=base_url()?>/assets/front/images/community/maker-space.jpg" alt="" class="img-fluid" alt="maker-space" class="img-fluid" style="height: 150px ; width: 200px;">
+						</a>
+					</div>
+					<div class="post-item-description">
+						<h2><a href="#">Maker Community
+							</a></h2>
+						<p>A Community intended to promote the maker culture among the students. Maker space running under the community provides basic infrastructure & resources for the students to transform their ideas in to reality.The Makers space equips modern machineries and rapid proto typing tools to support the young minds with advanced technological development</p>					
+					</div>
+				</div>
+			</div>
+
+</section>
