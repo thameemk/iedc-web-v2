90b59898a1829622e2a9838777e447c60f46b0cc
thameemk612 <thameemk612@gmail.com>
2019-12-20 22:22:38 +0530

    new login cred

---
+++ application/config/googleplus.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['googleplus']['application_name'] = '';
+$config['googleplus']['client_id']        = '140323478693-r43kj9jvll0396up5fbhqpp2apmmt9g4.apps.googleusercontent.com';
+$config['googleplus']['client_secret']    = 'mt-ZL_3nNu8WVicQBQGzFo_L';
+$config['googleplus']['redirect_uri']     = 'http://localhost/Login-with-google-plus-in-codeigniter/';
+$config['googleplus']['api_key']          = '';
+$config['googleplus']['scopes']           = array();
