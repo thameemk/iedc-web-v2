ab01cac1023db1929d071e6283fd94c8fd9cd405
thameemk612 <thameemk612@gmail.com>
2019-12-25 20:20:05 +0530

    auth redirect uri fix

--- application/config/googleplus.php
+++ application/config/googleplus.php
@@ -5,6 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['googleplus']['application_name'] = 'iedclocal.com';
 $config['googleplus']['client_id']        = '184229103860-jh8tpg261u3ma9450tohs279sgqoqto2.apps.googleusercontent.com';
 $config['googleplus']['client_secret']    = 'o9o4W1JyV-ZhfJgMVvrQNri5';
-$config['googleplus']['redirect_uri']     = 'http://iedclocal.com/iedc-web-v2/';
+$config['googleplus']['redirect_uri']     = 'http://iedclocal.com/iedc-web-v2/login';
 $config['googleplus']['api_key']          = '';
 $config['googleplus']['scopes']           = array();
