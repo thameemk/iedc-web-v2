68c4a81e91b711ac1b12a9d94a1b5a54edca2688
thameemk612 <thameemk612@gmail.com>
2019-12-26 15:29:04 +0530

    redirect uri

--- application/config/googleplus.php
+++ application/config/googleplus.php
@@ -2,9 +2,10 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['googleplus']['application_name'] = 'iedclocal.com';
+$config['googleplus']['application_name'] = 'iedctkmce.com';
 $config['googleplus']['client_id']        = '184229103860-jh8tpg261u3ma9450tohs279sgqoqto2.apps.googleusercontent.com';
 $config['googleplus']['client_secret']    = 'o9o4W1JyV-ZhfJgMVvrQNri5';
-$config['googleplus']['redirect_uri']     = 'http://iedclocal.com/iedc-web-v2/Auth/oauth2callback';
+// $config['googleplus']['redirect_uri']     = 'http://iedclocal.com/iedc-web-v2/Auth/oauth2callback';
+$config['googleplus']['redirect_uri']     = 'https://iedc.thameeem.com/Auth/oauth2callback';
 $config['googleplus']['api_key']          = '';
 $config['googleplus']['scopes']           = array();
