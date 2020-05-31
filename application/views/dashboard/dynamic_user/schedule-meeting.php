ffd3482249debaa380b2052f2c53f529c5559b3f
thameemk612 <thameemk612@gmail.com>
2020-05-31 16:44:07 +0530

    schedule meeting form

---
+++ application/views/dashboard/dynamic_user/schedule-meeting.php
@@ -0,0 +1,53 @@
+			<div class="page-content">
+
+				<nav class="page-breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="#">User</a></li>
+						<li class="breadcrumb-item active" aria-current="page">Schedule a meeting</li>
+					</ol>
+				</nav>
+
+				<div class="row">
+
+					<div class="col-md-6 grid-margin stretch-card">
+						<div class="card">
+							<div class="card-body">
+								<h6 class="card-title">Schedule a meeting</h6>
+								<form class="forms-sample" method="post" action="">
+									<div class="form-group row">
+										<label for="exampleInputUsername2" class="col-sm-3 col-form-label">Date</label>
+										<div class="col-sm-9">
+											<input type="date" class="form-control" id="date" placeholder="Date">
+										</div>
+									</div>
+									<div class="form-group row">
+										<label for="exampleInputEmail2" class="col-sm-3 col-form-label">Time</label>
+										<div class="col-sm-9">
+											<input type="email" class="form-control" id="time" autocomplete="off" placeholder="Time">
+										</div>
+                  </div>
+									<div class="form-group row">
+										<label for="exampleInputMobile" class="col-sm-3 col-form-label">Mobile</label>
+										<div class="col-sm-9">
+											<input type="number" class="form-control" id="phone" placeholder="Mobile number">
+										</div>
+									</div>
+									<div class="form-group row">
+                                        <label for="exampleFormControlTextarea1" class="col-sm-3 col-form-label">Meeting Agenda</label>
+                                        <textarea class="form-control" id="meeting_agenda" rows="5"></textarea>
+                                    </div>
+									<div class="form-group row">
+                                        <label for="exampleFormControlTextarea1" class="col-sm-3 col-form-label">Attendees</label>
+                                        <textarea class="form-control" id="attendeed" rows="5"></textarea>
+                                    </div>
+									<div class="form-group row">
+                                        <label for="exampleFormControlTextarea1" class="col-sm-3 col-form-label">Description</label>
+                                        <textarea class="form-control" id="description" rows="5"></textarea>
+                                    </div>
+									<button type="submit" class="btn btn-primary mr-2" disabled>Submit</button>
+									<button type="reset" class="btn btn-light">Reset</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
