9a38eb8ec5f558566ab0feafd25fa1ba9d5815cc
thameemk612 <thameemk612@gmail.com>
2020-06-05 11:27:08 +0530

    schedule meeting backend integration

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -61,18 +61,18 @@ class User_model extends CI_Model {
      }
 
      public function add_maker_request($data){
-      $this->db->insert('maker_lib_requests', $data);    
+      $this->db->insert('maker_lib_requests', $data);
      }
 
-     public function change_count_lib($data){   
-      $this->db->select ( 'available_count' );  
+     public function change_count_lib($data){
+      $this->db->select ( 'available_count' );
       $this->db->from('maker_library');
       $this->db->where('comp_num', $data);
       $query = $this->db->get();
       $num = $query->result_array();
-      $total_count = $num[0]['available_count'] - 1;      
-      // print_r($num);exit;   
-      $this->user_model->update_count($total_count,$data);      
+      $total_count = $num[0]['available_count'] - 1;
+      // print_r($num);exit;
+      $this->user_model->update_count($total_count,$data);
      }
 
      public function update_count($total_count,$data){
@@ -80,12 +80,38 @@ class User_model extends CI_Model {
        $temp = array(
         'available_count' => $total_count
        );
-       $this->db->update('maker_library', $temp);              
+       $this->db->update('maker_library', $temp);
      }
 
      public function maker_user_req($email){
-      $query = $this->db->query("select m.req_date,m.issue_date,m.return_date,m.req_component,c.name from maker_lib_requests m,maker_library c where c.comp_num = m.req_component and m.user_email= '".$email."' order by req_date DESC");       
+      $query = $this->db->query("select m.req_date,m.issue_date,m.return_date,m.req_component,c.name from maker_lib_requests m,maker_library c where c.comp_num = m.req_component and m.user_email= '".$email."' order by req_date DESC");
       return $query->result_array();
+     }
+
+     public function reg_schedule_meeting(){
+       $data = $this->input->post();
+       $data = $this->security->xss_clean($data);
+       $this->form_validation->set_rules('date','date','required');
+       $this->form_validation->set_rules('time','time','required');
+       $this->form_validation->set_rules('phone','phone','required');
+       $this->form_validation->set_rules('meeting_agenda','meeting_agenda ','required');
+       $this->form_validation->set_rules('attendees','attendees','required');
+       $this->form_validation->set_rules('description','description','required');
+       if($this->form_validation->run() == FALSE){
+          return false;
+       }else{
+         $data = array(
+           'user_email'=> $_SESSION['email'],
+           'date'=>$this->input->post('date'),
+           'time'=>$this->input->post('time'),
+           'phone'=>$this->input->post('phone'),
+           'meeting_agenda'=>$this->input->post('meeting_agenda'),
+           'attendees'=>$this->input->post('attendees'),
+           'description'=>$this->input->post('description'),
+         );
+         $this->db->insert('schedule_meeting', $data);
+         return true;
+       }
      }
 
 
--- application/views/dashboard/dynamic_user/schedule-meeting.php
+++ application/views/dashboard/dynamic_user/schedule-meeting.php
@@ -1,53 +1,58 @@
-			<div class="page-content">
+<div class="page-content">
 
-				<nav class="page-breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item"><a href="#">User</a></li>
-						<li class="breadcrumb-item active" aria-current="page">Schedule a meeting</li>
-					</ol>
-				</nav>
+  <nav class="page-breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="#">User</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Schedule a meeting</li>
+    </ol>
+  </nav>
 
-				<div class="row">
-
-					<div class="col-md-6 grid-margin stretch-card">
-						<div class="card">
-							<div class="card-body">
-								<h6 class="card-title">Schedule a meeting</h6>
-								<form class="forms-sample" method="post" action="">
-									<div class="form-group row">
-										<label for="exampleInputUsername2" class="col-sm-3 col-form-label">Date</label>
-										<div class="col-sm-9">
-											<input type="date" class="form-control" id="date" placeholder="Date">
-										</div>
-									</div>
-									<div class="form-group row">
-										<label for="exampleInputEmail2" class="col-sm-3 col-form-label">Time</label>
-										<div class="col-sm-9">
-											<input type="email" class="form-control" id="time" autocomplete="off" placeholder="Time">
-										</div>
-                  </div>
-									<div class="form-group row">
-										<label for="exampleInputMobile" class="col-sm-3 col-form-label">Mobile</label>
-										<div class="col-sm-9">
-											<input type="number" class="form-control" id="phone" placeholder="Mobile number">
-										</div>
-									</div>
-									<div class="form-group row">
-                                        <label for="exampleFormControlTextarea1" class="col-sm-3 col-form-label">Meeting Agenda</label>
-                                        <textarea class="form-control" id="meeting_agenda" rows="5"></textarea>
-                                    </div>
-									<div class="form-group row">
-                                        <label for="exampleFormControlTextarea1" class="col-sm-3 col-form-label">Attendees</label>
-                                        <textarea class="form-control" id="attendeed" rows="5"></textarea>
-                                    </div>
-									<div class="form-group row">
-                                        <label for="exampleFormControlTextarea1" class="col-sm-3 col-form-label">Description</label>
-                                        <textarea class="form-control" id="description" rows="5"></textarea>
-                                    </div>
-									<button type="submit" class="btn btn-primary mr-2" disabled>Submit</button>
-									<button type="reset" class="btn btn-light">Reset</button>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
+  <div class="row">
+    <div class="col-md-6 grid-margin stretch-card">
+      <div class="card">
+				<?php if($this->session->flashdata('fail')): ?>
+				<span style="line-height:3" class="badge badge-danger"><?php echo $this->session->flashdata('fail'); ?></span>
+				<?php endif; ?>
+				<?php if($this->session->flashdata('success')): ?>
+				<span style="line-height:3" class="badge badge-success"><?php echo $this->session->flashdata('success'); ?></span>
+				<?php endif; ?>
+        <div class="card-body">
+          <h6 class="card-title">Schedule a meeting</h6>
+          <form class="forms-sample" method="post" action="<?=base_url("user/schedule_meeting_post")?>">
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label">Date</label>
+              <div class="col-sm-9">
+                <input type="date" class="form-control" name="date" id="date" placeholder="Date" required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label">Time</label>
+              <div class="col-sm-9">
+                <input type="time" class="form-control" name="time" id="time" autocomplete="off" placeholder="Time" required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label">Mobile</label>
+              <div class="col-sm-9">
+                <input type="number" class="form-control" name="phone" id="phone" placeholder="Mobile number" required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label">Meeting Agenda</label>
+              <textarea class="form-control" name="meeting_agenda" id="meeting_agenda" rows="5" required></textarea>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label">Attendees</label>
+              <textarea class="form-control" name="attendees" id="attendees" rows="5" required></textarea>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label">Description</label>
+              <textarea class="form-control" name="description" id="description" rows="5" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary mr-2">Submit</button>
+            <button type="reset" class="btn btn-light">Reset</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
