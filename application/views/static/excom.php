9db35d10a633c5826c36eb02b71ac42a944c8a5b
thameemk612 <thameemk612@gmail.com>
2020-08-19 21:18:26 +0530

    excom page

--- application/views/static/excom.php
+++ application/views/static/excom.php
@@ -1,12 +1,12 @@
-<section id="page-title" data-parallax-image="<?=base_url()?>assets/front/images/banner/team.jpg">
+<section id="page-title" data-parallax-image="<?= base_url() ?>assets/front/images/banner/team.jpg">
     <div class="container">
         <div class="page-title">
             <h1>Meet the team</h1>
         </div>
         <div class="breadcrumb">
             <ul>
-                <li><a href="<?=base_url()?>">Home</a> </li>
-                <li class="active"><a href="<?=base_url()?>excom">Excom</a> </li>
+                <li><a href="<?= base_url() ?>">Home</a> </li>
+                <li class="active"><a href="<?= base_url() ?>excom">Excom</a> </li>
             </ul>
         </div>
     </div>
@@ -15,31 +15,39 @@
 
 <section>
     <div class="container">
-      <?php
-         foreach (array_reverse($execomYear) as $row) {
-      ?>
-        <div class="heading-text heading-line text-center">
-            <h3>Executive Committee <?=$row['execomYear']?></h3>
-        </div>
-        <div class="row team-members m-b-40">
-            <?php
-		           foreach ($execom as $col) {
-		        ?>
-            <?php if($row['execomYear']==$col['year']){ ?>
-            <div class="col-lg-3">
-                <div class="team-member">
-                    <div class="team-image">
-                        <img src="<?=base_url()?>assets/uploads/images/team/<?=$col['year']?>/<?=$col['imageLink']?>">
-                    </div>
-                    <div class="team-desc">
-                        <h3><?=$col['Name']?></h3>
-                        <p><?=$col['position']?></p>
-                    </div>
-                </div>
+        <?php
+        foreach (array_reverse($execomYear) as $row) {
+        ?>
+            <div class="heading-text heading-line text-center">
+                <h3>Executive Committee <?= $row['execomYear'] ?></h3>
             </div>
-            <?php } ?>
-            <?php } ?>
-        </div>
-    <?php } ?>
+            <div class="row team-members m-b-40">
+                <?php
+                foreach ($execom as $col) {
+                ?>
+                    <?php if ($row['execomYear'] == $col['year']) { ?>
+                        <div class="col-lg-3">
+                            <div class="team-member">
+                                <div class="team-image">
+                                    <img src="<?= base_url() ?>assets/uploads/images/team/<?= $col['year'] ?>/<?= $col['imageLink'] ?>">
+                                </div>
+                                <div class="team-desc">
+                                    <h3><?= $col['Name'] ?></h3>
+                                    <p><?= $col['position'] ?></p>
+                                    <span class="social-linkedin">
+                                        <?php if ($col['linkedin']) { ?>
+                                            <a class="mr-3" href="<?= $col['linkedin'] ?>" target="_blank"><i class="fab fa-linkedin"></i></a>
+                                        <?php }
+                                        if ($col['portfolio']) { ?>
+                                            <a href="<?= $col['portfolio'] ?>" target="_blank"><i class="fa fa-link"></i></a>
+                                        <?php } ?>
+                                        <span>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                <?php } ?>
+            </div>
+        <?php } ?>
     </div>
 </section>
