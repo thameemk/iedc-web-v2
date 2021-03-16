affc87968055f1c02aac47f6f8baba0b702bad84
thameemk612 <thameemk612@gmail.com>
2021-03-16 19:58:00 +0530

    project proposal

---
+++ application/views/static/project-proposal.php
@@ -0,0 +1,15 @@
+<div style="background: white;" id="slider" class="inspiro-slider" data-height="380">
+
+	<div class="slide">
+		<div class="container">
+			<div class="slide-captions text-center text-dark">
+
+				<h3>Invitation for</h3>
+				<h2>PROJECT PROPOSALS</h2>
+
+			</div>
+		</div>
+	</div>
+
+</div>
+
