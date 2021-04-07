d07e30f0d35de3b855b7f32c4edc77b4e05cfafb
thameemk612 <thameemk612@gmail.com>
2021-04-07 13:06:12 +0530

    hyper link blank

--- application/views/static/event-registration.php
+++ application/views/static/event-registration.php
@@ -25,17 +25,17 @@
         <ol style="font-size:1.3em !important;letter-spacing:1px;">
             <li> Please login or access your profile
                 <?php if ($this->session->userdata('sess_logged_in') == 0) { ?>
-                    <a style="color:#007bff !important;" href="<?php echo $loginURL ?>">[ Click here ]</a>
+                    <a style="color:#007bff !important;"  href="<?php echo $loginURL ?>" target="_blank">[ Click here ]</a>
                 <?php } else { ?>
-                    <a style="color:#007bff !important;" href="<?= base_url() ?>user/dashboard">[ Click here ]</a>
+                    <a style="color:#007bff !important;" href="<?= base_url() ?>user/dashboard" target="_blank">[ Click here ]</a>
                 <?php } ?>
             </li>
             <li>Complete the profile if not done</li>
             <li>Go to Events & program section
                 <?php if ($this->session->userdata('sess_logged_in') == 0) { ?>
-                    <a style="color:#007bff !important;" href="<?php echo $loginURL ?>">[ Click here ]</a>
+                    <a style="color:#007bff !important;" href="<?php echo $loginURL ?>" target="_blank">[ Click here ]</a>
                 <?php } else { ?>
-                    <a style="color:#007bff !important;" href="<?= base_url() ?>user/dashboard/events">[ Click here ]</a>
+                    <a style="color:#007bff !important;" href="<?= base_url() ?>user/dashboard/events" target="_blank">[ Click here ]</a>
                 <?php } ?>
             </li>
             <li>Find the event or program that you want register</li>
