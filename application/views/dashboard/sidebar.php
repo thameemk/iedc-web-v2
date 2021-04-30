2034c148482f00fda4a7afd4d59f375f05f23afe
thameemk612 <thameemk612@gmail.com>
2021-04-30 19:19:51 +0530

    event management

--- application/views/dashboard/sidebar.php
+++ application/views/dashboard/sidebar.php
@@ -130,6 +130,16 @@
 								<span class="link-title">Pre incubation requests</span>
 							</a>
 						</li>
+
+						<li class="nav-item">
+							<a href="<?= base_url() ?>admin/dashboard/event-management" class="nav-link">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square link-icon">
+									<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+								</svg>
+								<span class="link-title">Event Management</span>
+							</a>
+						</li>
+
 						<li class="nav-item">
 							<a href="<?= base_url() ?>admin/dashboard/volunteer-database" class="nav-link">
 								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square link-icon">
