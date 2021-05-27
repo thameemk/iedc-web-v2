ca5eeb2fd56e4b1d48edcbca06f6de29abbe8322
thameemk612 <thameemk612@gmail.com>
2021-05-27 16:15:37 +0530

    error file extension

--- application/models/Upload_file.php
+++ application/models/Upload_file.php
@@ -6,12 +6,12 @@ class Upload_file extends CI_Model
         $this->load->database();
     }
 
-    function do_upload($path, $file_name)
+    function do_upload($path, $file_name,$file_type)
     {
         $config['upload_path']          =  $path;
         $config['allowed_types']        =  'png|jpeg|jpg';
         $config['max_size']             = 2048;
-        $file_name = time().mt_rand();
+        $file_name = time().mt_rand().'.'.$file_type;
         $config['file_name'] = $file_name;
         $this->load->library('upload', $config);
         if (!$this->upload->do_upload('userfile')) {
