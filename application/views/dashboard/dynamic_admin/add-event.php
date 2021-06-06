f0d99052c3033996c03d1d078ab74b7ab87fc153
thameemk612 <thameemk612@gmail.com>
2021-06-06 23:56:36 +0530

    add evint

--- application/views/dashboard/dynamic_admin/add-event.php
+++ application/views/dashboard/dynamic_admin/add-event.php
@@ -214,7 +214,7 @@
                                     </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                                <button class="btn btn-light">Cancel</button>
+                                <button type="reset" class="btn btn-light">Cancel</button>
                             </form>
                         </div>
                     </div>
